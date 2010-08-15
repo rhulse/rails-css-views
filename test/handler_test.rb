4dc4138f4b1b9baea2864ac4d1c3ac4e8b51a472
Michael Koziarski <michael@koziarski.com>
2010-08-16 09:24:29 +1200

    First overly mockish tests

---
+++ test/handler_test.rb
@@ -0,0 +1,47 @@
+require 'test_helper'
+
+class HandlerTest < Test::Unit::TestCase 
+  context "A simple handler" do
+    setup do
+      @controller = stub_controller
+      @controller.expects(:render_to_string).with("main").returns("FAKE_BODY").once
+      @configuration = CssProcessor::Configuration.new("test_config", :components=>["main"])
+      @handler = CssProcessor::Handler.new(@configuration, @controller)
+    end
+    
+    should "return the body unmodified when rendering" do
+      assert_equal "FAKE_BODY", @handler.render
+    end
+  end
+  
+  context "A handler with two components" do
+    setup do
+      @controller = stub_controller
+      @controller.expects(:render_to_string).with("main").returns("FAKE_BODY").once
+      @controller.expects(:render_to_string).with("admin").returns("ADMIN_BODY").once
+      @configuration = CssProcessor::Configuration.new("test_config", :components=>["main", "admin"])
+      @handler = CssProcessor::Handler.new(@configuration, @controller)
+    end
+    
+    should "return the concatenated body" do
+      assert_equal "FAKE_BODY\nADMIN_BODY", @handler.render
+    end
+  end
+  
+  
+  context "A handler with two components and a processor" do
+    setup do
+      @controller = stub_controller
+      @controller.expects(:render_to_string).with("main").returns("FAKE_BODY").once
+      @controller.expects(:render_to_string).with("admin").returns("ADMIN_BODY").once
+      @configuration = CssProcessor::Configuration.new("test_config", :components=>["main", "admin"], :transformers=>[DownCaseTransformer.new])
+      @handler = CssProcessor::Handler.new(@configuration, @controller)
+    end
+    
+    should "return the concatenated body after transformation" do
+      assert_equal "fake_body\nadmin_body", @handler.render
+    end
+  end
+  
+  
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,22 @@
+require 'rubygems'
+gem 'actionpack', '>= 3.0.0.rc'
+
+require 'active_support'
+require 'shoulda'
+require 'mocha'
+require 'css_processor'
+
+
+class Test::Unit::TestCase
+  def stub_controller
+    stub.tap do |controller|
+      controller
+    end
+  end
+end
+
+class DownCaseTransformer
+  def transform(css)
+    css.downcase
+  end
+end
