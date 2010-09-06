b6e0b668531a126787ff358f589c0e4a9b2c59af
Richard Hulse <richard.hulse@radionz.co.nz>
2010-09-06 15:18:21 +1200

    patch to disable caching in development

--- lib/css_views/controller_mixins.rb
+++ lib/css_views/controller_mixins.rb
@@ -12,23 +12,22 @@ module CssViews
         self.configurations[name.to_s] = Configuration.new(name, options)
       end
     end
-    
+
     module InstanceMethods
       def show
         configuration = self.class.configurations[params[:configuration_name]]
         head :not_found unless configuration
         
         handler = configuration.handler(self)
-        
-        fresh_when(:etag=>handler.etag, :last_modified=>handler.last_modified, :public=>true)
-
-        unless performed?
+        if perform_caching
+          fresh_when(:etag=>handler.etag, :last_modified=>handler.last_modified, :public=>true)
           if params[:cache_buster]
             response.headers['Cache-Control']= 'public, max-age=131557600'
             response.headers['Expires'] = 10.years.from_now.httpdate
           end
-          render :text=>handler.render, :type=>Mime::CSS 
+          return if performed?
         end
+        render :text=>handler.render, :type=>Mime::CSS 
       end
     end
   end
